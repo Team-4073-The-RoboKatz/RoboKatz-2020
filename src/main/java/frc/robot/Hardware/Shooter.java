f79f5feadd1453c73a5f912336d0a521e7ed41bb
hkuperman42 <hkuperman42@gmail.com>
2022-02-28 15:38:43 -0500

    initial commit

--- src/main/java/frc/robot/Hardware/Shooter.java
+++ src/main/java/frc/robot/Hardware/Shooter.java
@@ -5,10 +5,12 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.robot.Constants;
 
 public class Shooter {
+    //Create hardware objects
     public TalonSRX m_intake;
     public TalonSRX m_shooter;
 
     public Shooter() {
+        //Initialize hardware objects
         m_shooter = new TalonSRX(Constants.shooterPort);
         m_intake = new TalonSRX(Constants.intakePort);
     }
