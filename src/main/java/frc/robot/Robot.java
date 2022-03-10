275a1099153f554a7d35946fd49bc46d366e9c95
dockerteen <timmybud922@gmail.com>
2022-03-10 00:03:32 -0500

    Added to Auto.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -116,16 +116,34 @@ public class Robot extends TimedRobot {
 
     shooter.m_index.set(ControlMode.PercentOutput, 0);
     shooter.m_shooter.set(0);
+
     //Turn around (180º..maybe)
     drivetrain.mechanumDriveTime(0, 1, 0, 500);
+
     //Drive straight for 2(..seconds?) and turns on the intake
-    shooter.m_intake.set(ControlMode.PercentOutput,   (9.75));
+    shooter.m_intake.set(ControlMode.PercentOutput, (.75));
     drivetrain.mechanumDriveTime(1, 0, 0, 2000);
+
     //Turn the robot 180, Turn on the shooter, Drive back to the starting line. (TRY TO OMMIT WITH SHOOTER TWEAKING) 
-    drivetrain.mechanumDriveTime(0, 1, 0, 500);    
-    shooter.m_shooter.set(.75);
-    drivetrain.mechanumDriveTime(1, 0, 0, 2000);
+
+    drivetrain.mechanumDriveTime(-1, 0, 0, 2000);
+    shooter.m_shooter.set(highshooterpower);
+    drivetrain.mechanumDriveTime(0, 1, 0, 500);
+
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
     shooter.m_index.set(ControlMode.PercentOutput, (.50));
+    //WAIT//
+    //Turns OFF shooter/index/intake
+    
+    shooter.m_index.set(ControlMode.PercentOutput, (0));
+    shooter.m_intake.set(ControlMode.PercentOutput, (0));
+    shooter.m_shooter.set(0);
   }
 
   /** This function is called periodically during autonomous. */
